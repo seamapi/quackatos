4a6f3bb774daacd8ab7c53236fe8072d3bb99412
Max Isom <hi@maxisom.me>
2022-08-02 13:52:44 -0500

    Add tests for UPDATE

---
+++ src/lib/builders/commands/update.test.ts
@@ -0,0 +1,72 @@
+import test from "ava"
+import { assert, Equals } from "tsafe"
+import { createMacroForBuilder, getTestDatabase } from "~/tests"
+import * as schema from "zapatos/schema"
+import { UpdateCommand } from "./update"
+
+const macro = createMacroForBuilder({
+  builderFactory: () => new UpdateCommand("film"),
+})
+
+test(
+  "set() (object)",
+  macro,
+  (builder) => builder.set({ title: "foo" }),
+  async (t, _, pool) => {
+    const result = await pool.query(
+      "SELECT title FROM film WHERE title = 'foo' LIMIT 1"
+    )
+    t.is(result.rows.length, 1)
+  }
+)
+
+test(
+  "set() (single column)",
+  macro,
+  (builder) => builder.set("title", "foo"),
+  async (t, _, pool) => {
+    const result = await pool.query(
+      "SELECT title FROM film WHERE title = 'foo' LIMIT 1"
+    )
+    t.is(result.rows.length, 1)
+  }
+)
+
+test("returning() (array)", macro, (builder) =>
+  builder.set({ title: "foo" }).whereIn("film_id", [1]).returning(["title"])
+)
+
+test("returning() (rest parameter)", macro, (builder) =>
+  builder
+    .set({ title: "foo" })
+    .whereIn("film_id", [1])
+    .returning("title", "film_id")
+)
+
+test("returning()", macro, (builder) =>
+  builder.set({ title: "foo" }).whereIn("film_id", [1]).returning("film_id")
+)
+
+test("returning() (*, typed correctly)", async (t) => {
+  const { pool } = await getTestDatabase()
+  const result = await new UpdateCommand("film")
+    .set({ title: "foo" })
+    .returning("*")
+    .run(pool)
+
+  assert<Equals<typeof result[0], schema.film.Selectable>>()
+  t.pass()
+})
+
+test("returning() (two columns, typed correctly)", async (t) => {
+  const { pool } = await getTestDatabase()
+  const result = await new UpdateCommand("film")
+    .set({ title: "foo" })
+    .returning(["film_id", "title"])
+    .run(pool)
+
+  assert<
+    Equals<typeof result[0], Pick<schema.film.Selectable, "film_id" | "title">>
+  >()
+  t.pass()
+})
--- src/lib/builders/commands/update.ts
+++ src/lib/builders/commands/update.ts
@@ -3,23 +3,30 @@ import * as schema from "zapatos/schema"
 import { mix } from "ts-mixer"
 import { WhereableStatement } from "../common/where"
 import { SQLCommand } from "../types"
+import {
+  ColumnSpecificationsForTable,
+  constructColumnSelection,
+  SelectableForTableFromColumnSpecifications,
+} from "./utils/construct-column-selection"
 
 export interface UpdateCommand<
   TableName extends schema.Table,
   Updatable = schema.UpdatableForTable<TableName>,
-  Whereable = schema.WhereableForTable<TableName>
+  Whereable = schema.WhereableForTable<TableName>,
+  Returning = never
 > extends WhereableStatement<Whereable>,
-    SQLCommand<void> {}
+    SQLCommand<Returning> {}
 
 @mix(WhereableStatement, SQLCommand)
 export class UpdateCommand<
   TableName extends schema.Table,
   Updatable = schema.UpdatableForTable<TableName>,
-  Whereable = schema.WhereableForTable<TableName>
+  Whereable = schema.WhereableForTable<TableName>,
+  Returning = never
 > {
   private readonly _tableName: string
   private _values: Partial<Updatable> = {}
-  private _returning: (keyof schema.SelectableForTable<TableName>)[] = []
+  private _returning: ColumnSpecificationsForTable<TableName>[] = []
 
   constructor(tableName: TableName) {
     this._tableName = tableName
@@ -32,6 +39,7 @@ export class UpdateCommand<
       this._values[args[0] as keyof Updatable] = args[1]
     } else if (args.length === 1) {
       this._values = {
+        // todo: should this merge?
         ...args[0],
       }
     }
@@ -39,17 +47,43 @@ export class UpdateCommand<
     return this
   }
 
-  // todo: support *
-  returning<T extends (keyof schema.SelectableForTable<TableName>)[]>(
-    columnNames: T
-  ): this {
-    this._returning = columnNames
-    return this
+  returning<T extends ColumnSpecificationsForTable<TableName>>(
+    columnSpecifications: T[]
+  ): UpdateCommand<
+    TableName,
+    Updatable,
+    Whereable,
+    SelectableForTableFromColumnSpecifications<TableName, T>
+  >
+  returning<T extends ColumnSpecificationsForTable<TableName>>(
+    ...columnNames: T[]
+  ): UpdateCommand<
+    TableName,
+    Updatable,
+    Whereable,
+    SelectableForTableFromColumnSpecifications<TableName, T>
+  >
+  returning<T extends ColumnSpecificationsForTable<TableName>>(
+    ...args: any
+  ): UpdateCommand<
+    TableName,
+    Updatable,
+    Whereable,
+    SelectableForTableFromColumnSpecifications<TableName, T>
+  > {
+    if (args.length === 1 && Array.isArray(args[0])) {
+      this._returning = args[0]
+    } else {
+      this._returning = args
+    }
+    return this as any
   }
 
   compile() {
     const returningSQL =
-      this._returning.length > 0 ? sql`RETURNING ${cols(this._returning)}` : []
+      this._returning.length > 0
+        ? sql`RETURNING ${constructColumnSelection(this._returning)}`
+        : []
 
     return sql`UPDATE ${this._tableName} SET (${cols(
       this._values
