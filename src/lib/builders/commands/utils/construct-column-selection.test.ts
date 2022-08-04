f1a545a03effc162fc7892490d42b5cc99dedd79
Max Isom <hi@maxisom.me>
2022-08-04 16:31:32 -0500

    Add type test

---
+++ src/lib/builders/commands/utils/construct-column-selection.test.ts
@@ -0,0 +1,26 @@
+import test from "ava"
+import { assert, Equals } from "tsafe"
+import { ColumnSpecificationsForTable } from "./construct-column-selection"
+
+test("typed correctly", (t) => {
+  type PossibleSpecifications = ColumnSpecificationsForTable<"film" | "actor">
+
+  // Allow * notation
+  assert<Equals<Extract<PossibleSpecifications, "*">, "*">>()
+
+  // Allow TableName.* notation
+  assert<Equals<Extract<PossibleSpecifications, "film.*">, "film.*">>()
+
+  // Allow TableName.ColumnName notation
+  assert<
+    Equals<Extract<PossibleSpecifications, "film.film_id">, "film.film_id">
+  >()
+
+  // Allow ColumnName notation
+  assert<Equals<Extract<PossibleSpecifications, "film_id">, "film_id">>()
+
+  // These tables share the column name last_update, so it should not be part of the union
+  assert<Equals<Extract<PossibleSpecifications, "last_update">, never>>()
+
+  t.pass()
+})
