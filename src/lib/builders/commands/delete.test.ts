5b4c45aa8c7632088bb2b692781259b4ddd2f31b
Max Isom <hi@maxisom.me>
2022-08-05 10:55:38 -0500

    Add basic DELETE command

---
+++ src/lib/builders/commands/delete.test.ts
@@ -0,0 +1,28 @@
+import test from "ava"
+import { assert, Equals } from "tsafe"
+import { getTestDatabase } from "~/tests"
+import { DeleteCommand } from "./delete"
+
+test("delete works", async (t) => {
+  const { pool } = await getTestDatabase()
+
+  const {
+    rows: [{ count: countBeforeDelete }],
+  } = await pool.query(
+    `SELECT COUNT(*) FROM film_actor WHERE film_id IN (1, 2, 3)`
+  )
+  t.not(parseInt(countBeforeDelete, 10), 0)
+
+  const result = await new DeleteCommand("film_actor")
+    .whereIn("film_id", [1, 2, 3])
+    .run(pool)
+
+  assert<Equals<typeof result, Array<never>>>()
+
+  const {
+    rows: [{ count: countAfterDelete }],
+  } = await pool.query(
+    `SELECT COUNT(*) FROM film_actor WHERE film_id IN (1, 2, 3)`
+  )
+  t.is(parseInt(countAfterDelete, 10), 0)
+})
---
+++ src/lib/builders/commands/delete.ts
@@ -0,0 +1,28 @@
+import { mix } from "ts-mixer"
+import { sql } from "zapatos/db"
+import * as schema from "zapatos/schema"
+import { WhereableStatement } from "../common"
+import { SQLCommand } from "../types"
+
+export interface DeleteCommand<
+  TableName extends schema.Table,
+  Whereable extends schema.WhereableForTable<TableName>
+> extends WhereableStatement<Whereable>,
+    SQLCommand<never> {}
+
+@mix(WhereableStatement, SQLCommand)
+export class DeleteCommand<
+  TableName extends schema.Table,
+  Whereable extends schema.WhereableForTable<TableName>
+> {
+  private readonly _tableName: string
+
+  constructor(tableName: TableName) {
+    this._tableName = tableName
+  }
+
+  // todo: don't allow running if _whereable is empty
+  compile() {
+    return sql`DELETE FROM ${this._tableName} WHERE ${this._whereable}`.compile()
+  }
+}
