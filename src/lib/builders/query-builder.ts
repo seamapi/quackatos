515733729ea0742bab112becb070981d06743d9e
Max Isom <hi@maxisom.me>
2022-08-08 18:58:01 -0500

    fix: trigger release

--- src/lib/builders/query-builder.ts
+++ src/lib/builders/query-builder.ts
@@ -1,4 +1,8 @@
 import * as schema from "zapatos/schema"
 import { SelectCommand } from "./commands"
 
-export class QueryBuilder<T extends schema.Table> extends SelectCommand<T> {}
+export class QueryBuilder<T extends schema.Table> extends SelectCommand<T> {
+  constructor(tableName: T) {
+    super(tableName)
+  }
+}
