eece35bffc937193d39515d91215c10e4c06b59a
Max Isom <hi@maxisom.me>
2022-08-12 12:25:41 -0500

    Add spinner

--- src/scripts/get-terminal-to-test-database.ts
+++ src/scripts/get-terminal-to-test-database.ts
@@ -1,23 +1,33 @@
 import path from "node:path"
 import child_process from "node:child_process"
+import ora from "ora"
 import getTestDatabase from "~/tests/fixtures/workers/helpers/get-test-database"
 
 const getTerminalToTestDatabase = async () => {
-  const { databaseName, container } = await getTestDatabase(
-    path.join(__dirname, "../tests/assets")
-  )
+  const spinner = ora("Booting up database...").start()
+
+  let fixture
+
+  try {
+    fixture = await getTestDatabase(path.join(__dirname, "../tests/assets"))
+  } catch (error) {
+    spinner.fail((error as Error).message)
+    throw error
+  }
+
+  spinner.stop()
 
   child_process.spawn(
     "docker",
     [
       "exec",
       "-it",
-      container.getId(),
+      fixture.container.getId(),
       "psql",
       "-U",
       "postgres",
       "-d",
-      databaseName,
+      fixture.databaseName,
     ],
     { stdio: "inherit" }
   )
